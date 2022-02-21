1b67226eb5fb634052af05cbfb6430f35c0a351c
pedrocambier <pedrocambier@gmail.com>
2022-02-21 01:21:56 -0300

    refactoring RPS game to use UI and DOM manipulation

--- body.js
+++ body.js
@@ -1,7 +1,6 @@
 const RPS = ['rock', 'paper', 'scissors'];
 
 function roundRPS(playerSelection, computerSelection) {
-  playerSelection = playerSelection.toLowerCase();
   if (playerSelection === computerSelection) return 0;
   if (playerSelection === 'rock') return (computerSelection === 'scissors') ? 1 : -1;
   if (playerSelection === 'paper') return (computerSelection === 'rock') ? 1 : -1;
@@ -9,25 +8,29 @@ function roundRPS(playerSelection, computerSelection) {
   return -1;
 }
 
-function game() {
-  let scorePlayer = 0;
-  let scoreComputer = 0;
-  const maxRounds = 5;
-  for (let i=0; i<maxRounds; i++){
-    const computerSelection = RPS[Math.floor(Math.random()*3)];
-    let playerSelection = (window.prompt('Enter your choice: ')).toLowerCase(); 
-    while (!RPS.includes(playerSelection)) {
-      playerSelection = (window.prompt('Invalid choice! Enter your choice: ')).toLowerCase(); 
-    }
-    console.log(`Player choose ${playerSelection} and computer choose ${computerSelection}.`);
+let scorePlayer = 0;
+let scoreComputer = 0;
+let numRounds = 0;
+const maxRounds = 5;
+const buttons = document.querySelectorAll('button');
+const div = document.querySelector('div');
+div.style.whiteSpace = "pre";
+Array.from(buttons).forEach(button => {
+  button.addEventListener('click', event => {
+    const computerSelection = RPS[Math.floor(Math.random() * 3)];
+    let playerSelection = event.target.textContent;
     const resultRound = roundRPS(playerSelection, computerSelection);
-    console.log(`${(resultRound == 1) ? "You Win!" : (resultRound == 0) ? "It's a tie!" : "You lose!"} ${playerSelection} ${(resultRound == 1) ? "beats" : (resultRound == 0) ? "is equal to" : "loses to"} ${computerSelection}!`);
+    div.textContent = `${(resultRound == 1) ? "You Win!" : (resultRound == 0) ? "It's a tie!" : "You lose!"} ${playerSelection} ${(resultRound == 1) ? "beats" : (resultRound == 0) ? "is equal to" : "loses to"} ${computerSelection}!`;
     scoreComputer += (resultRound === -1);
     scorePlayer += (resultRound === 1);
-    console.log(`This is round ${i+1}, score is Player: ${scorePlayer} Computer ${scoreComputer}.`);
-  }
-
-  console.log('Game has Finished!');
-  console.log(`Final score is Player: ${scorePlayer} Computer ${scoreComputer}.`);
-  console.log(`${(scorePlayer===scoreComputer) ? "It's a draw!!" : (scorePlayer > scoreComputer) ? "Congratulations, you win!" : "Too bad, you lose!"}`);
-}
+    div.textContent += "\r\n" + `This is round ${++numRounds}, score is Player: ${scorePlayer} Computer ${scoreComputer}.`;
+    if (numRounds === 5) {
+      div.textContent += "\r\nGame has Finished!";
+      div.textContent += "\r\n" + `Final score is Player: ${scorePlayer} Computer ${scoreComputer}.`;
+      div.textContent += "\r\n" + `${(scorePlayer === scoreComputer) ? "It's a draw!!" : (scorePlayer > scoreComputer) ? "Congratulations, you win!" : "Too bad, you lose!"}`;
+      scorePlayer = 0;
+      scoreComputer = 0;
+      numRounds = 0;
+    }
+  });
+});
